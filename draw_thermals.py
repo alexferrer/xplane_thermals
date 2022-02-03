0e3b25bf6d3dce58344bc1153d94c4551bb2a988
Alex Ferrer <ferrerdallas@gmail.com>
2022-02-03 12:22:25 -0500

    Working version thermal columns/cloudbases

--- draw_thermals.py
+++ draw_thermals.py
@@ -10,66 +10,72 @@ SMALL_CLOUD = 'lib/airport/landscape/windsock.obj'
 # 'lib/airport/Common_Elements/Markers/Poles/Large_Red_White.obj'
 THERMAL_COLUMN = 'lib/dynamic/seagull_glide.obj'
 
+# thermal image
+thermal_column = xp.loadObject(
+    'Resources/default scenery/sim objects/dynamic/SailBoat.obj')
 
-def DrawThermal(lat, lon):  # min_alt,max_alt
-    ''' make a location list of thermal images along the raising thermal, accounting
-        for wind drift along the climb. end at thermal tops
+# cloudbase_image
+paths = []
+print("paths", paths)
+xp.lookupObjects(HUGE_CLOUD, 0, 0, lambda path,
+                 refCon: paths.append(path), None)
+print("paths", paths)
+huge_cloud = xp.loadObject(paths[0])
+print("huge-cloud", huge_cloud)
+
+
+def draw_thermal(lat, lon):  # min_alt,max_alt
+    ''' Draw thermal images along the raising thermal, accounting
+        for wind drift along the climb. end at almost the thermal top
     '''
     base = 1
-    Dew, Dud, Dns = xp.worldToLocal(lat, lon, 0)  # Dew=E/W,Dud=Up/Down,Dns=N/S
-    locs = []  # locations
-    # from 100 to thermal top steps of  200
-    for alt in range(base, world.thermal_tops, 200):
-        dX, dY = calcDrift(alt)
-        locs.append([Dew + dX, Dud + alt, Dns + dY, 0, 0, 0])
-    return locs
+    _dew, _dud, _dns = xp.worldToLocal(
+        lat, lon, 0)  # Dew=E/W,Dud=Up/Down,Dns=N/S
+    # from 100 to almost thermal top steps of 200
+    for alt in range(base, world.thermal_tops-200, 200):
+        _dx, _dy = calcDrift(alt)
+        instance = xp.createInstance(thermal_column)
+        xp.instanceSetPosition(
+            instance, [_dew + _dx, _dud + alt, _dns + _dy, 0, 0, 0])
+        world.instance_list.append(instance)
 
 
-def DrawCloud(lat, lon):
+def draw_cloud(lat, lon):
     ''' Return the location for a cloud at the top of the raising thermal, accounting
         for wind drift along the climb.
     '''
-    Dew, Dud, Dns = xp.worldToLocal(lat, lon, 0)  # Dew=E/W,Dud=Up/Down,Dns=N/S
-    locs = []  # locations
-    alt = world.thermal_tops
-    dX, dY = calcDrift(alt)
-    locs.append((Dew + dX, Dud + alt, Dns + dY, 0, 0, 0))
-
-    return locs
-
-
-''' return a list of thermal location tuples, if the distance not exceeds max display 
-    draw the thermal column of rising air
-'''
+    # Dew=E/W,Dud=Up/Down,Dns=N/S
+    _dew, _dud, _dns = xp.worldToLocal(lat, lon, 0)
+    _alt = world.thermal_tops
+    _dx, _dy = calcDrift(_alt)
+    instance = xp.createInstance(huge_cloud)
+    xp.instanceSetPosition(
+        instance, [_dew + _dx, _dud + _alt, _dns + _dy, 0, 0, 0])
+    world.instance_list.append(instance)
 
 
-def DrawThermalMap(lat, lon):
-    locations = []
-    p1x, p1y = convertLatLon2Meters(lat, lon)
+def draw_thermal_columns(lat, lon):
+    ''' return a list of thermal location tuples, if the distance not exceeds max display
+        draw the thermal column of rising air
+    '''
+    _p1x, _p1y = convertLatLon2Meters(lat, lon)
 
     for athermal in world.thermal_dict:
         p2x, p2y = athermal.px, athermal.py
-        if calcDist(p1x, p1y, p2x, p2y) < world.max_draw_distance:
-            locations.extend(DrawThermal(athermal.lat, athermal.lon))
-
-    return locations
+        if calcDist(_p1x, _p1y, p2x, p2y) < world.max_draw_distance:
+            draw_thermal(athermal.lat, athermal.lon)
 
 
-''' return a list of thermal location tuples, if the distance not exceeds max display 
+def draw_clouds(lat, lon):
+    ''' return a list of thermal location tuples, if the distance not exceeds max display
     Just the thermals at cloudbase
-'''
-
-
-def DrawCloudMap(lat, lon):
-    locations = []
+    '''
     p1x, p1y = convertLatLon2Meters(lat, lon)
 
     for athermal in world.thermal_dict:
         p2x, p2y = athermal.px, athermal.py
         if calcDist(p1x, p1y, p2x, p2y) < world.max_draw_distance:
-            locations.extend(DrawCloud(athermal.lat, athermal.lon))
-
-    return locations
+            draw_cloud(athermal.lat, athermal.lon)
 
 
 def eraseThermalsOnScreen():
@@ -83,57 +89,14 @@ def eraseThermalsOnScreen():
 
 
 def drawThermalsOnScreen(lat, lon):
-    if world.DEBUG:
-        print("DrawThermalsOnScreen:start")
-
     # if visibility is off, only draw clouds at cloudbase (no visible columns)
-
-    if world.thermals_visible:
-        locs = DrawThermalMap(lat, lon)
-    else:
-        locs = DrawCloudMap(lat, lon)
-
-    if world.DEBUG:
-        print("plane ( lat, lon ) ", lat, lon)
-        print("plane local", xp.worldToLocal(lat, lon, 0))
-        #print("thermal location = ",loc[0])
-
-    paths = []
-    xp.lookupObjects(HUGE_CLOUD, 0, 0,
-                     lambda path, refCon: paths.append(path), None)
-    huge_cloud = xp.loadObject(paths[0])
-
-    # print(paths)
-    thermal_column = xp.loadObject(
-        'Resources/default scenery/sim objects/dynamic/SailBoat.obj')
-
-    #print("object1", huge_cloud)
-    #print("object2", thermal_column)
 
     eraseThermalsOnScreen()
 
-    if world.DEBUG:
-        print("loop: create instances and position")
-    _p = True
-    for loc in locs:
-        # select the object to draw
-        _p = not _p
-        if _p:
-            instance = xp.createInstance(huge_cloud)
-            #print("huge cloud")
-        else:
-            instance = xp.createInstance(thermal_column)
-            # print("thermal_column")
+    draw_clouds(lat, lon)
 
-        if world.DEBUG:
-            print("loop: xp.instanceSetPosition(instance,", instance, loc)
-        xp.instanceSetPosition(instance, loc)
-        world.instance_list.append(instance)
+    if world.thermals_visible:
+        draw_thermal_columns(lat, lon)
 
     world.world_update = False
     return 1
-
-
-def select_cloud_type(_thermal):
-    ''' return a cloud type based on the thermal size and power'''
-    print(_thermal)
